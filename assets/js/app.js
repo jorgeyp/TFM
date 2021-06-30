681715a55983c99eb68ad4a0bb41108229ddc23c
Jorge Yagüe París <jorgeyp@gmail.com>
2021-06-30 21:00:26 +0200

    Add root icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
+import "./all.js"
 
 const Hooks = {}
 Hooks.UpdateScroll = {
